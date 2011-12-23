acf8ef3e01acc9b9fad1af70602ab1515ae226bd
john.macinroe <john.macinroe@7582a24c-f007-11de-a8d5-974f63da035b>
2011-12-23 13:05:07 +0000

    Updated to version 2.8.4. See changes.log.

--- publish-air.bat
+++ publish-air.bat
@@ -1,3 +1,2 @@
 @echo off
-call ..\AIR\SDK\bin\adt -package -storetype pkcs12 -keystore C:\Documents\Security\AIR-JosephCrowe.pfx published\package.air  application.xml content modules locale icons changes.log
-pause
+call ..\AIR\SDK\bin\adt -package -storetype pkcs12 -keystore C:\Documents\Security\AIR-JosephCrowe.pfx published\package.air  application.xml content modules locale icons changes.log || pause
