270615859b65b99266547ab16ee8e9d06678605e
john.macinroe <john.macinroe@7582a24c-f007-11de-a8d5-974f63da035b>
2014-06-10 17:01:48 +0000

    Upgrade to new certificate.

--- trunk/publish-air-no_tsa.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-call ..\AIR_SDK\bin\adt -package -tsa none -storetype pkcs12 -keystore ..\Certificate\AIR-JosephCrowe.pfx published\package.air  application.xml content modules locale icons changes.log || pause
